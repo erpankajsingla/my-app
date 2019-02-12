144da5cb9867f1425bdaf51fd125ec66b8a48a2f
Pankaj Singla <psingla@605.tv>
2019-02-12 11:02:26 -0500

    Sample recipe app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { RootComponent } from './root/root.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { EditShoppingListComponent } from './shopping-list/edit-shopping-list/edit-shopping-list.component';
+import { IngredientComponent } from './shopping-list/ingredient/ingredient.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RootComponent,
+    ShoppingListComponent,
+    EditShoppingListComponent,
+    IngredientComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipesComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
